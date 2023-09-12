356f3fff0e8a789ebcfddde06566ddf9314079de
Marawan-Osama <marawanegy@gmail.com>
2023-09-12 21:17:42 +0300

    added job routes and applied in app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 const authRouter = require("./routes/auth");
-
+const jobsRouter = require("./routes/jobs");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -13,6 +13,7 @@ app.use(express.json());
 
 //routes
 app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/jobs", jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
