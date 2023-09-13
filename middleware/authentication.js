0f30b707411e6062a88c4263a554344f3468db85
Marawan-Osama <marawanegy@gmail.com>
2023-09-13 15:16:54 +0300

    added log for header

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,6 +4,7 @@ const { UnauthenticatedError } = require('../errors')
 
 const auth = async (req, res, next) => {
   // check header
+  console.log(req.headers)
   const authHeader = req.headers.authorization
   if (!authHeader || !authHeader.startsWith('Bearer')) {
     throw new UnauthenticatedError('Authentication invalid')
